a113cf2169fd6981c36878c4e50e1d9adf524f32
BaranBaranDev <auniversty@gmail.com>
2024-07-21 16:04:25 +0300

    feat: Add TipInputView and Tip Model

---
+++ tip-calculator/tip-calculator/Model/Tip.swift
@@ -0,0 +1,32 @@
+//
+//  Tip.swift
+//  tip-calculator
+//
+//  Created by Baran Baran on 21.07.2024.
+//
+
+
+import Foundation
+
+enum Tip {
+  case none
+  case tenPercent
+  case fifteenPercent
+  case twentyPercent
+  case custom(value: Int)
+  
+  var stringValue: String {
+    switch self {
+    case .none:
+      return ""
+    case .tenPercent:
+      return "10%"
+    case .fifteenPercent:
+      return "15%"
+    case .twentyPercent:
+      return  "20%"
+    case .custom(let value):
+      return String(value)
+    }
+  }
+}
